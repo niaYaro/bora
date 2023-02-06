d71755abd1f575eeb620eb23019b29d9b4d9590b
niaYaro <yarovyi@gmail.com>
2023-02-06 18:40:06 +0200

    code cleaned

--- js/responsive.js
+++
@@ -1,9 +0,0 @@
-// console.log('width', window.innerWidth);
-// if (window.innerWidth > 374) {
-//     firstProduct.classList.remove('hidden');
-//     secondProduct.classList.remove('hidden');
-//     thirdProduct.classList.remove('hidden');
-//     fourthProduct.classList.remove('hidden');
-//     fifthProduct.classList.remove('hidden');
-//     sixthProduct.classList.remove('hidden');
-// }
