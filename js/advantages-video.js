b2559a79eaab5b6e0119799eabd7201c3b53b890
niaYaro <yarovyi@gmail.com>
2023-02-06 12:56:08 +0200

    finished show advantages video feature

--- js/advantages-video.js
+++ js/advantages-video.js
@@ -6,11 +6,19 @@ const advantagesFirstVideoPlay = document.getElementById('navigation-1');
 const advantagesSecondVideoPlay = document.getElementById('navigation-2');
 const advantagesThirdVideoPlay = document.getElementById('navigation-3');
 const advantagesFourthVideoPlay = document.getElementById('navigation-4');
+const advantagesMobileFirstVideoPlay = document.getElementById('mobile-navigation-1');
+const advantagesMobileSecondVideoPlay = document.getElementById('mobile-navigation-2');
+const advantagesMobileThirdVideoPlay = document.getElementById('mobile-navigation-3');
+const advantagesMobileFourthVideoPlay = document.getElementById('mobile-navigation-4');
 
 advantagesFirstVideoPlay.addEventListener('click', showMovie(advantagesFirstVideo));
+advantagesMobileFirstVideoPlay.addEventListener('click', showMovie(advantagesFirstVideo));
 advantagesSecondVideoPlay.addEventListener('click', showMovie(advantagesSecondVideo));
+advantagesMobileSecondVideoPlay.addEventListener('click', showMovie(advantagesSecondVideo));
 advantagesThirdVideoPlay.addEventListener('click', showMovie(advantagesThirdVideo));
+advantagesMobileThirdVideoPlay.addEventListener('click', showMovie(advantagesThirdVideo));
 advantagesFourthVideoPlay.addEventListener('click', showMovie(advantagesFourthVideo));
+advantagesMobileFourthVideoPlay.addEventListener('click', showMovie(advantagesFourthVideo));
 
 function showMovie(video) {
     return function() {
